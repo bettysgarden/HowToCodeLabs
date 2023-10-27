a6a5322be047fc9ad28cad4edb860a0531bb2d23
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-10-27 12:33:46 +0300

    Update Library.cs

--- HardcodingProblematic/Library.cs
+++ HardcodingProblematic/Library.cs
@@ -1,26 +1,34 @@
 namespace HardcodingProblematic;
+
 public class Library
 {
     private List<Book> books = new();
 
-    // Плохая читабельность
-
-    // Жесткость
-    // использование классов напрямую без интерфейсов
     public void Add(Book book)
     {
         books.Add(book);
     }
 
-    // Жесткость
-    // использование классов напрямую без интерфейсов
-    public void Remove(Book book)
-    {
-        books.Remove(book);
-    }
-
     public List<Book> GetBooks()
     {
         return books;
+    }
+
+    public bool IsBookAvailable(Book book, bool check1, bool check2, bool check3)
+    {
+        bool isAvailable = books.Contains(book);
+        if (check1)
+        {
+            isAvailable = !isAvailable;
+        }
+        if (check2)
+        {
+            isAvailable = !isAvailable;
+        }
+        if (check3)
+        {
+            isAvailable = !isAvailable;
+        }
+        return isAvailable;
     }
 }
