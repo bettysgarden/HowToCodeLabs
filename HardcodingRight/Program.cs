73e50d01675691665667bffc7a874f09c24a77d9
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-10-27 12:56:04 +0300

    Update Program.cs

--- HardcodingRight/Program.cs
+++ HardcodingRight/Program.cs
@@ -9,25 +9,25 @@ class Program
     {
         ILibrary library = new Library();
 
-        IBook book1 = new Book("Зима тревоги нашей", "Джон Стейнбек");
+        IItem book1 = new Book("Зима тревоги нашей", "Джон Стейнбек");
         library.AddBook(book1);
 
-        IBook book2 = new Book("Убить пересмешника", "Харпер Ли");
+        IItem book2 = new Book("Убить пересмешника", "Харпер Ли");
         library.AddBook(book2);
 
         library.SetBookAvailability(book1, false); // Книгу 1 сделали недоступной через библиотеку
         library.SetBookAvailability(book2, true);  // Книгу 2 сделали доступной через библиотеку
 
         bool isAvailable = library.IsBookAvailable(book1);
-        Console.WriteLine("Книга 1 доступнаe: " + isAvailable);
+        Console.WriteLine("Книга 1 доступна: " + isAvailable);
 
         isAvailable = library.IsBookAvailable(book2);
         Console.WriteLine("Книга 2 доступна: " + isAvailable);
 
-        List<IBook> books = library.GetBooks();
+        List<IItem> books = library.GetBooks();
 
         Console.WriteLine("Книги в библиотеке:");
-        foreach (IBook book in books)
+        foreach (IItem book in books)
         {
             Console.WriteLine($"Название: {book.Title}, Автор: {book.Author}");
         }
