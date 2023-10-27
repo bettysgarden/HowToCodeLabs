7cc946d3bbcdb971c6ff5538667c5a7d20fb3a20
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-10-27 12:33:56 +0300

    Update Program.cs

--- HardcodingProblematic/Program.cs
+++ HardcodingProblematic/Program.cs
@@ -8,11 +8,24 @@ class Program
     {
         Library library = new Library();
 
-        Book book1 = new Book("Зима тревоги нашей", "Джон Стейнбек");
-        library.Add(book1);
+        Book book1 = new Book("Зима тревоги нашей", "Джон Стейнбек", library);
+        //library.Add(book1);
+        book1.AddToLibrary(book1);
 
-        Book book2 = new Book("Убить пересмешника", "Харпер Ли");
-        library.Add(book2);
+        Book book2 = new Book("Убить пересмешника", "Харпер Ли", library);
+        //library.Add(book2);
+        book2.AddToLibrary(book2);
+
+
+ bool checkAvailability = true;
+        if (checkAvailability)
+        {
+            bool isAvailable = CheckAvailabilityWithConfusion(library, book1);
+            Console.WriteLine("Book 1 is available: " + isAvailable);
+
+            isAvailable = CheckAvailabilityWithConfusion(library, book2);
+            Console.WriteLine("Book 2 is available: " + isAvailable);
+        }
 
         List<Book> books = library.GetBooks();
 
