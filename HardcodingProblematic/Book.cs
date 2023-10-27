8e771a2dd3395e020da42e0facdfabe4174e0d16
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-10-27 12:33:52 +0300

    Update Book.cs

--- HardcodingProblematic/Book.cs
+++ HardcodingProblematic/Book.cs
@@ -2,12 +2,23 @@ namespace HardcodingProblematic;
 
 public class Book
 {
+    // Жесткость: отсутствие интерфейса
+        // Например: аудиокниги
     public string Title { get; }
     public string Author { get; }
+    private Library library; // Нарушение связности: Класс Book имеет прямую ссылку на Library.
 
-    public Book(string title, string author)
+    public Book(string title, string author, Library library)
     {
         Title = title;
         Author = author;
+        this.library = library;
+    }
+
+    // неожиданное поведение -- книга сама добавляет себя в библиотеку
+    public void AddToLibrary(Book book) 
+    {
+        library.Add(book);
     }
 }
+
