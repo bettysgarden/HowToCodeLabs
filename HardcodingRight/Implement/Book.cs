dd09e41f2491665a4291e560c335fff6b3fd3784
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-10-27 12:54:58 +0300

    Update Book.cs

--- HardcodingRight/Implement/Book.cs
+++ HardcodingRight/Implement/Book.cs
@@ -4,7 +4,6 @@ public class Book : IItem
 {
     public string Title { get; }
     public string Author { get; }
-    private bool Availability = true; // По умолчанию книга доступна
 
     public Book(string title, string author)
     {
