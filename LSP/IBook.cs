dc57493693adc0f9d5d87ca7d166f14acf35fb4d
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-11-16 18:27:12 +0300

    Update and rename IBook.cs to IReadable.cs

--- LSP/IBook.cs
+++
@@ -1,8 +0,0 @@
-namespace LSP;
-
-// Интерфейс, представляющий книгу
-public interface IBook
-{
-    string Title { get; set; }
-    void Read();
-}
---
+++ LSP/IReadable.cs
@@ -0,0 +1,7 @@
+namespace LSP;
+
+// Интерфейс для читаемых ресурсов в библиотеке
+public interface IReadable
+{
+    string Read();
+}
