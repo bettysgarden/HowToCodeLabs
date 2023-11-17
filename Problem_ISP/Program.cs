4b619d43d60abb810001386fab53727e94fca024
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-11-17 10:15:52 +0300

    Update Program.cs

--- Problem_ISP/Program.cs
+++ Problem_ISP/Program.cs
@@ -3,17 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reader reader = new Reader();
-        
-        // Создаём экземпляр аудиокниги
-        Book audioBook = new AudioBook();
-
-        // Передаём экземпляр аудиокниги функции, ожидая чтения книги
-        // Наследующий класс должен дополнять, а не замещать поведение базового класса.
-				// при переопределении мы изменили поведение исходного класса
-				// кого куда нельзя подставить?
-				reader .ReadBook(audioBook);
-
-        Console.ReadLine();
+        Library book = new Library();
+        User user = new User();
+        user.ProcessBook(book); // Все методы из интерфейса вызываются, хотя не все нужны пользователю
     }
 }
